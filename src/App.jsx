35f34be91d12a7a0e7015f1e3fa277387eabcf4a
leekjoo <leekjoo1008@gmail.com>
2025-03-04 22:20:59 +0900

    refector: 중복 코드  삭제

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,29 @@ import { useState } from "react";
 import "./App.css";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
-import { Log } from "./components/Log";
+import Log from "./components/Log";
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  // 기본적으로 currentPlayer은 X이다.
+  // 이전이 X라면 currentPlayer은 O이다.
+  return currentPlayer;
+}
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+
+  //현재 누가 플레이어인지 UI에 반영하는 용도
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   const handlegetSelectedSquare = (rowIndex, colIndex) => {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
-
+    //버튼을 누른 후
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-      //게임을 한번이라도 시작했거나, 최신 플레이어가 "X" 라면
-      if (gameTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      //현재 상태가 아닌, 이전 상태(prevTurns) 기준으로 activePlayer 계산
+      const currentPlayer = deriveActivePlayer(prevTurns);
       const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns];
       return updatedTurns;
     });

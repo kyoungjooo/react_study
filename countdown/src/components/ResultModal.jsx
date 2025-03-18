7b07054ff5944c6b98eb66321994b911809118ff
kyoungjooo <leekjoo1008@gmail.com>
2025-03-18 19:37:45 +0900

    feat: 게임 종료 및 초기화 상태 설정하기

--- countdown/src/components/ResultModal.jsx
+++ countdown/src/components/ResultModal.jsx
@@ -1,6 +1,19 @@
+import { useImperativeHandle, useRef } from "react";
+
 export default function ResultModal({ ref, result, targetTime }) {
+  const dialog = useRef();
+
+  const handleOpen = () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  };
+  useImperativeHandle(ref, handleOpen);
+
   return (
-    <dialog ref={ref} className="result-modal">
+    <dialog ref={dialog} className="result-modal">
       <h2>You {result}</h2>
       <p>
         The target timer is {targetTime} second{targetTime > 1 ? "s" : ""}
--- countdown/src/components/TimerChallenge.jsx
+++ countdown/src/components/TimerChallenge.jsx
@@ -2,21 +2,28 @@ import { useRef, useState } from "react";
 import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }) {
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
-  const timerRef = useRef(); //컴포넌트 내부에 있기 때문에 특정 컴포넌트 인스컨스에만 할당
+  const timerRef = useRef(); //컴포넌트 내부에 있기 때문에 특정 컴포넌트 인스턴스에만 할당
   const modalRef = useRef();
 
+  //컴포넌트가 처음 렌더링 될 때 초기값을 설정
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  // timeRemaining 0 이되도 게임 종료 초기화
+  if (timeRemaining <= 0) {
+    clearInterval(timerRef.current);
+    setTimeRemaining(targetTime * 1000);
+  }
+
   const handleStartTimer = () => {
-    timerRef.current = setTimeout(() => {
-      setTimerExpired(true); // n초 후 타이머 종료
-      modalRef.current.showModal();
-    }, targetTime * 1000);
+    timerRef.current = setInterval(() => {
+      setTimeRemaining((prev) => prev - 10);
+    }, 10);
   };
 
+  //stop 버튼을 눌렀을 때
   const handleStopTimer = () => {
-    clearTimeout(timerRef.current);
-    setTimerStarted(false);
+    clearInterval(timerRef.current);
   };
 
   return (
@@ -24,17 +31,17 @@ export default function TimerChallenge({ title, targetTime }) {
       <ResultModal ref={modalRef} targetTime={targetTime} result="lost" />
       <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>game is over!</p>}
+        {timerIsActive && <p>game is over!</p>}
         <div className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </div>
         <div>
-          <button onClick={timerStarted ? handleStopTimer : handleStartTimer}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStopTimer : handleStartTimer}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </div>
-        <div className={timerStarted ? "active" : undefined}>
-          <p>{timerStarted ? "Time is running..." : "Timer inactive"}</p>
+        <div className={timerIsActive ? "active" : undefined}>
+          <p>{timerIsActive ? "Time is running..." : "Timer inactive"}</p>
         </div>
       </section>
     </>

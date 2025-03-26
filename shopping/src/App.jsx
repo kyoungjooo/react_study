3aedf3becaabb437bd85e24c9f2527473ad50a94
kyoungjooo <leekjoo1008@gmail.com>
2025-03-26 21:29:29 +0900

    feat: 컨텍스트 추가

--- shopping/src/App.jsx
+++ shopping/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
-
-import Header from './components/Header.jsx';
-import Shop from './components/Shop.jsx';
-import { DUMMY_PRODUCTS } from './dummy-products.js';
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.js";
+import Product from "./components/Product.jsx";
+import { CartContext } from "../../countdown/src/store/cartContext.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -13,11 +14,15 @@ function App() {
     setShoppingCart((prevShoppingCart) => {
       const updatedItems = [...prevShoppingCart.items];
 
+      //아이디가 같은 카트 아이템 인덱스 찾기
       const existingCartItemIndex = updatedItems.findIndex(
         (cartItem) => cartItem.id === id
       );
+
+      //찾은 인덱스에 해당하는 배열 요소를 existingCartItem에 저장
       const existingCartItem = updatedItems[existingCartItemIndex];
 
+      //있으면 개수를 1개 추가
       if (existingCartItem) {
         const updatedItem = {
           ...existingCartItem,
@@ -25,6 +30,7 @@ function App() {
         };
         updatedItems[existingCartItemIndex] = updatedItem;
       } else {
+        //없으면 제품을 추가
         const product = DUMMY_PRODUCTS.find((product) => product.id === id);
         updatedItems.push({
           id: id,
@@ -64,15 +70,27 @@ function App() {
       };
     });
   }
+  //자식 컴포넌트들이 쓸 전역 상태(장바구니 + 함수)를 묶은 객체
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
 
   return (
-    <>
+    //value로 전달한 ctxValue가 실제 Context 값
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
-    </>
+      <Shop onAddItemToCart={handleAddItemToCart}>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} onAddToCart={handleAddItemToCart} />
+          </li>
+        ))}
+      </Shop>
+    </CartContext.Provider>
   );
 }
 

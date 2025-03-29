b9b4442a7c8e4036498b7c9927716ca8d696e665
kyoungjooo <leekjoo1008@gmail.com>
2025-03-29 20:23:35 +0900

    feat: useEffect로 open 값이 변경될때마다 동기화

--- placepicker/src/components/Modal.jsx
+++ placepicker/src/components/Modal.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
 export default function Modal({ children, open }) {
+  const dialogRef = useRef(); //ref 자체는 렌더링 결과에 영향을 주는 게 아님
+  console.log(dialogRef.current); //undefined
+  //렌더링이 완료된 후에 dialogRef.current에 접근 가능
+
+  useEffect(() => {
+    if (open) {
+      dialogRef.current.showModal(); //dialogRef.current가 null이 아님
+    } else {
+      dialogRef.current.close();
+    }
+  }, [open]); //open이 바뀔 때마다 실행
+
   return createPortal(
-    <dialog className="modal" open={open}>
+    //화면에 보이는 내용이나 JSX 구조 자체를 변경하지 않는다
+    <dialog className="modal" ref={dialogRef}>
       {children}
     </dialog>,
     document.getElementById("modal")

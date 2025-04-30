4371ef159b9d6d418b7ffc3e307da09145d4676f
kyoungjooo <leekjoo1008@gmail.com>
2025-04-30 22:36:50 +0900

    feat: control error

--- demoform/src/components/Login.jsx
+++ demoform/src/components/Login.jsx
@@ -1,17 +1,61 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+
+  const handleInputBlur = (name) => {
+    setDidEdit((prevEdit) => ({ ...prevEdit, [name]: true }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(enteredValues);
+  };
+
+  const handleInputChange = (name, value) => {
+    setEnteredValues((prev) => ({ ...prev, [name]: value }));
+    setDidEdit((prevEdit) => ({ ...prevEdit, [name]: false }));
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onBlur={() => handleInputBlur("email")}
+            onChange={(e) => handleInputChange("email", e.target.value)}
+            value={enteredValues.email}
+          />
+          <div className="control-error">
+            {emailIsInvalid && <p>유효한 이메일 주소를 입력해주세요</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onBlur={() => handleInputBlur("password")}
+            onChange={(e) => handleInputChange("password", e.target.value)}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 

3df013b9f10372624ac9dba7fe79364ec96a24ab
kyoungjooo <leekjoo1008@gmail.com>
2025-05-08 17:07:37 +0900

    컴포넌트에서 리덕스 사용

--- reduxreact/src/components/Counter.js
+++ reduxreact/src/components/Counter.js
@@ -1,13 +1,25 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useDispatch, useSelector } from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
-
+  //redux store에 대한 action 을 보내는 함수
+  const dispatch = useDispatch();
+  //state는 Redux store 전체 상태 객체, 그 중에서 counter라는 값만 뽑아오는 선택 함수
+  const counter = useSelector((state) => state.counter);
+  const increaseHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={increaseHandler}>increment</button>
+        <button onClick={decrementHandler}>decrement</button>
+      </div>
     </main>
   );
 };
